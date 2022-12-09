8682efcf35cf57a98678819fc3ea45ac209bfebd
Bochuan Gao <1295670385@qq.com>
2022-12-09 22:46:50 +0800

    done

--- app/src/main/java/top/bochuangao/myapplicatio/MainActivity.java
+++ app/src/main/java/top/bochuangao/myapplicatio/MainActivity.java
@@ -3,12 +3,28 @@ package top.bochuangao.myapplicatio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        tv1 = findViewById(R.id.result);
+
+        btn1.setOnClickListener(view -> {
+            int a = 10;
+            Random random = new Random();
+            int b = random.nextInt(a);
+            tv1.setText("" + b);
+        });
     }
 }
